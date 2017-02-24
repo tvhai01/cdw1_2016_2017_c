d7c0f1b59a9ad11b3d7ff63f198f8cd9e8d0369a
tvhai01 <runordie98@gmail.com>
2017-02-24 16:27:14 +0700

    update database

--- database/dev_laravel_v53.sql
+++ database/dev_laravel_v53.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2017 at 03:55 AM
+-- Generation Time: Feb 24, 2017 at 09:25 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -19,8 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `dev_laravel_v53`
 --
-CREATE DATABASE IF NOT EXISTS `dev_laravel_v53` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `dev_laravel_v53`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_main`
+--
+
+CREATE TABLE `authors_main` (
+  `authors_main_id` int(11) NOT NULL,
+  `authors_main_description` varchar(255) DEFAULT NULL,
+  `authors_main_img_1` varchar(255) DEFAULT NULL,
+  `authors_main_img_2` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,9 +50,29 @@ CREATE TABLE `catalogs` (
 -- Dumping data for table `catalogs`
 --
 
-INSERT INTO `catalogs` (`catalog_id`, `catalog_name`, `catalog_parentid`, `catalog_sortoder`) VALUES
+INSERT DELAYED INTO `catalogs` (`catalog_id`, `catalog_name`, `catalog_parentid`, `catalog_sortoder`) VALUES
 (1, 'name1', 1, 10),
 (2, 'name2', 2, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `gallery_id` int(11) NOT NULL,
+  `gallery_title` varchar(45) DEFAULT NULL,
+  `gallery_description` varchar(255) DEFAULT NULL,
+  `gallery_img_1` varchar(255) DEFAULT NULL,
+  `gallery_img_2` varchar(255) DEFAULT NULL,
+  `gallery_img_3` varchar(255) DEFAULT NULL,
+  `gallery_img_4` varchar(255) DEFAULT NULL,
+  `gallery_img_5` varchar(255) DEFAULT NULL,
+  `gallery_img_6` varchar(255) DEFAULT NULL,
+  `gallery_img_7` varchar(255) DEFAULT NULL,
+  `gallery_img_8` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,10 +93,21 @@ CREATE TABLE `groups` (
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `name`, `permissions`, `protected`, `created_at`, `updated_at`) VALUES
+INSERT DELAYED INTO `groups` (`id`, `name`, `permissions`, `protected`, `created_at`, `updated_at`) VALUES
 (1, 'superadmin', '{"_superadmin":1}', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
 (2, 'editor', '{"_user-editor":1,"_group-editor":1}', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
 (3, 'base admin', '{"_user-editor":1}', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logo`
+--
+
+CREATE TABLE `logo` (
+  `logo_id` int(11) NOT NULL,
+  `logo_img` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -83,7 +125,7 @@ CREATE TABLE `migrations` (
 -- Dumping data for table `migrations`
 --
 
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+INSERT DELAYED INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2012_12_06_225988_migration_cartalyst_sentry_install_throttle', 1),
 (2, '2014_02_19_095545_create_users_table', 1),
 (3, '2014_02_19_095623_create_user_groups_table', 1),
@@ -111,7 +153,7 @@ CREATE TABLE `news` (
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`new_id`, `new_image`, `new_title`, `new_description`) VALUES
+INSERT DELAYED INTO `news` (`new_id`, `new_image`, `new_title`, `new_description`) VALUES
 (1, 'img1', 'img1_1', 'abc'),
 (2, 'img2', 'img2_2', 'abcd');
 
@@ -146,7 +188,7 @@ CREATE TABLE `permission` (
 -- Dumping data for table `permission`
 --
 
-INSERT INTO `permission` (`id`, `description`, `permission`, `protected`, `created_at`, `updated_at`) VALUES
+INSERT DELAYED INTO `permission` (`id`, `description`, `permission`, `protected`, `created_at`, `updated_at`) VALUES
 (1, 'superadmin', '_superadmin', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
 (2, 'user editor', '_user-editor', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
 (3, 'group editor', '_group-editor', 0, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
@@ -175,7 +217,7 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `product_catalogid`, `product_name`, `product_price`, `product_discount`, `product_imagelink`, `product_imagelist`, `product_created`, `product_view`) VALUES
+INSERT DELAYED INTO `products` (`product_id`, `product_catalogid`, `product_name`, `product_price`, `product_discount`, `product_imagelink`, `product_imagelist`, `product_created`, `product_view`) VALUES
 (1, 2, 'product1', 4, 10, 'abcde', 'abcde', 1, 2),
 (2, 3, 'abcde', 5, 9, 'abcede', 'abcde', 2, 3);
 
@@ -210,6 +252,36 @@ CREATE TABLE `profile_field_type` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `question_1`
+--
+
+CREATE TABLE `question_1` (
+  `question_1_id` int(11) NOT NULL,
+  `question_1_title_1` varchar(255) DEFAULT NULL,
+  `question_1_description_1` varchar(255) DEFAULT NULL,
+  `question_1_title_2` varchar(255) DEFAULT NULL,
+  `question_1_description_2` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_2`
+--
+
+CREATE TABLE `question_2` (
+  `question_2_id` int(11) NOT NULL,
+  `question_2_title_3` varchar(255) DEFAULT NULL,
+  `question_2_description_3` varchar(255) DEFAULT NULL,
+  `question_2_title_4` varchar(255) DEFAULT NULL,
+  `question_2_description_4` varchar(255) DEFAULT NULL,
+  `question_2_title_5` varchar(255) DEFAULT NULL,
+  `question_2_description_5` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `samples`
 --
 
@@ -222,7 +294,7 @@ CREATE TABLE `samples` (
 -- Dumping data for table `samples`
 --
 
-INSERT INTO `samples` (`sample_id`, `sample_name`) VALUES
+INSERT DELAYED INTO `samples` (`sample_id`, `sample_name`) VALUES
 (1, 'tranviethai'),
 (2, 'phamanhnguyen'),
 (3, 'nguyenminhthai'),
@@ -245,7 +317,7 @@ CREATE TABLE `slideshows` (
 -- Dumping data for table `slideshows`
 --
 
-INSERT INTO `slideshows` (`slideshow_id`, `slideshow_name`, `slideshow_img`) VALUES
+INSERT DELAYED INTO `slideshows` (`slideshow_id`, `slideshow_name`, `slideshow_img`) VALUES
 (1, 'slideshow1', 'abcde'),
 (2, 'slideshow2', 'abcdef');
 
@@ -271,8 +343,22 @@ CREATE TABLE `throttle` (
 -- Dumping data for table `throttle`
 --
 
-INSERT INTO `throttle` (`id`, `user_id`, `ip_address`, `attempts`, `suspended`, `banned`, `last_attempt_at`, `suspended_at`, `banned_at`) VALUES
+INSERT DELAYED INTO `throttle` (`id`, `user_id`, `ip_address`, `attempts`, `suspended`, `banned`, `last_attempt_at`, `suspended_at`, `banned_at`) VALUES
 (1, 1, '127.0.0.1', 0, 0, 0, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `title_mains`
+--
+
+CREATE TABLE `title_mains` (
+  `title_main_id` int(11) NOT NULL,
+  `title_main_title` varchar(255) DEFAULT NULL,
+  `title_main_content` varchar(255) DEFAULT NULL,
+  `title_main_description` varchar(255) DEFAULT NULL,
+  `title_main_video` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -296,7 +382,7 @@ CREATE TABLE `transactions` (
 -- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`transaction_id`, `transaction_status`, `transaction_first_name`, `transaction_last_name`, `transaction_phone`, `amount`, `payment`, `payment_info`, `created`) VALUES
+INSERT DELAYED INTO `transactions` (`transaction_id`, `transaction_status`, `transaction_first_name`, `transaction_last_name`, `transaction_phone`, `amount`, `payment`, `payment_info`, `created`) VALUES
 (1, 'transaction_status', 'first_name	', 'last_name', '123456789', '30', 'abcdefghijklmnop', 'abcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnop', 1),
 (2, 'transaction_status2', 'first_name2', 'last_name2', '123789456', '23', 'sdasdasssasdsada', 'abcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnopabcdefghijklmnop', 2);
 
@@ -327,8 +413,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `permissions`, `activated`, `banned`, `activation_code`, `activated_at`, `last_login`, `persist_code`, `reset_password_code`, `protected`, `created_at`, `updated_at`) VALUES
-(1, 'admin@admin.com', '$2y$10$8OWc3y5qDAPGkd/G/vr1Je4pYtqlDKRt4SJonbiXKb6vA.AaJVZPK', NULL, 1, 0, NULL, NULL, '2017-02-23 04:32:20', '$2y$10$rrM0xLyXzwDMWKdNRrLVauYCt0/eigzqRVUXu.jbQB4J2CAmPz80q', NULL, 0, '2017-02-16 19:07:34', '2017-02-23 04:32:20'),
+INSERT DELAYED INTO `users` (`id`, `email`, `password`, `permissions`, `activated`, `banned`, `activation_code`, `activated_at`, `last_login`, `persist_code`, `reset_password_code`, `protected`, `created_at`, `updated_at`) VALUES
+(1, 'admin@admin.com', '$2y$10$8OWc3y5qDAPGkd/G/vr1Je4pYtqlDKRt4SJonbiXKb6vA.AaJVZPK', NULL, 1, 0, NULL, NULL, '2017-02-23 23:07:59', '$2y$10$EUMOJ8JLRy/37Fr1gRlKCeZygDA2bx2HONsc/Je.D4/9pZKEp7g52', NULL, 0, '2017-02-16 19:07:34', '2017-02-23 23:07:59'),
 (2, 'phuc@admin.com', '$2y$10$Sx2vWTY0SvQDWdGcJqC4R.xCmGbkb47c4ULuP0Mn5R9cBAQNn2kt2', NULL, 1, 0, NULL, NULL, NULL, NULL, NULL, 0, '2017-02-23 04:34:42', '2017-02-23 04:35:15'),
 (3, 'nguyen@admin.com', '$2y$10$P/ylSrOqKZ5QYUqywNIw2eDpJ42D495gPIyFqY9IQ6UJmvuKm1CTm', NULL, 1, 0, NULL, NULL, NULL, NULL, NULL, 0, '2017-02-23 04:35:43', '2017-02-23 04:35:43'),
 (4, 'thai@admin.com', '$2y$10$fbe98.U.Ffi0RL7IHZEPaOqB47W6eoT7IBtfRKFhbUKnuYiWioB/G', NULL, 1, 0, NULL, NULL, NULL, NULL, NULL, 0, '2017-02-23 04:36:18', '2017-02-23 04:36:18');
@@ -348,7 +434,7 @@ CREATE TABLE `users_groups` (
 -- Dumping data for table `users_groups`
 --
 
-INSERT INTO `users_groups` (`user_id`, `group_id`) VALUES
+INSERT DELAYED INTO `users_groups` (`user_id`, `group_id`) VALUES
 (1, 1);
 
 -- --------------------------------------------------------
@@ -379,7 +465,7 @@ CREATE TABLE `user_profile` (
 -- Dumping data for table `user_profile`
 --
 
-INSERT INTO `user_profile` (`id`, `user_id`, `code`, `vat`, `first_name`, `last_name`, `phone`, `state`, `city`, `country`, `zip`, `address`, `avatar`, `created_at`, `updated_at`) VALUES
+INSERT DELAYED INTO `user_profile` (`id`, `user_id`, `code`, `vat`, `first_name`, `last_name`, `phone`, `state`, `city`, `country`, `zip`, `address`, `avatar`, `created_at`, `updated_at`) VALUES
 (1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-02-16 19:07:34', '2017-02-16 19:07:34'),
 (2, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-02-23 04:34:42', '2017-02-23 04:34:42'),
 (3, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2017-02-23 04:35:43', '2017-02-23 04:35:43'),
@@ -390,10 +476,22 @@ INSERT INTO `user_profile` (`id`, `user_id`, `code`, `vat`, `first_name`, `last_
 --
 
 --
+-- Indexes for table `authors_main`
+--
+ALTER TABLE `authors_main`
+  ADD PRIMARY KEY (`authors_main_id`);
+
+--
 -- Indexes for table `catalogs`
 --
 ALTER TABLE `catalogs`
   ADD PRIMARY KEY (`catalog_id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`gallery_id`);
 
 --
 -- Indexes for table `groups`
@@ -401,6 +499,12 @@ ALTER TABLE `catalogs`
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `groups_name_unique` (`name`);
+
+--
+-- Indexes for table `logo`
+--
+ALTER TABLE `logo`
+  ADD PRIMARY KEY (`logo_id`);
 
 --
 -- Indexes for table `migrations`
@@ -448,6 +552,18 @@ ALTER TABLE `profile_field_type`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `question_1`
+--
+ALTER TABLE `question_1`
+  ADD PRIMARY KEY (`question_1_id`);
+
+--
+-- Indexes for table `question_2`
+--
+ALTER TABLE `question_2`
+  ADD PRIMARY KEY (`question_2_id`);
+
+--
 -- Indexes for table `samples`
 --
 ALTER TABLE `samples`
@@ -465,6 +581,12 @@ ALTER TABLE `slideshows`
 ALTER TABLE `throttle`
   ADD PRIMARY KEY (`id`),
   ADD KEY `throttle_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `title_mains`
+--
+ALTER TABLE `title_mains`
+  ADD PRIMARY KEY (`title_main_id`);
 
 --
 -- Indexes for table `transactions`
@@ -499,15 +621,30 @@ ALTER TABLE `user_profile`
 --
 
 --
+-- AUTO_INCREMENT for table `authors_main`
+--
+ALTER TABLE `authors_main`
+  MODIFY `authors_main_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `catalogs`
 --
 ALTER TABLE `catalogs`
   MODIFY `catalog_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `gallery_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `logo`
+--
+ALTER TABLE `logo`
+  MODIFY `logo_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
@@ -529,6 +666,16 @@ ALTER TABLE `profile_field`
 ALTER TABLE `profile_field_type`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `question_1`
+--
+ALTER TABLE `question_1`
+  MODIFY `question_1_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `question_2`
+--
+ALTER TABLE `question_2`
+  MODIFY `question_2_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `samples`
 --
 ALTER TABLE `samples`
@@ -538,6 +685,11 @@ ALTER TABLE `samples`
 --
 ALTER TABLE `throttle`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `title_mains`
+--
+ALTER TABLE `title_mains`
+  MODIFY `title_main_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `transactions`
 --
