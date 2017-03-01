9b49d4b71677fc7d3b4fcaeb607475112244d5f8
tvhai01 <runordie98@gmail.com>
2017-03-01 09:33:01 +0700

    update laravel admin

---
+++ laravel/packages/foostart/sample/src/lang/en/sample_admin.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    "package_name" => 'Sample',
+    "package_description" => 'Sample package is for initial',
+    "order" => '#',
+    "operations" => 'Operations',
+
+    /**
+     * Page
+     */
+
+    'page_list' => 'Sample page list',
+    'page_add' => 'Sample page add',
+    'page_edit' => 'Sample page edit',
+    'page_search' => 'Sample page search',
+
+    /**
+     * Menu items & sidebar
+     */
+    'list' => 'List of samples',
+    'add' => 'Add new item',
+    'edit' => 'Update sample item',
+
+    /**
+     * Form
+     */
+    'form_heading' => 'General data',
+    'form_add' => 'Add new sample item',
+    'form_edit' => 'Update sample item',
+    'name' => 'Name',
+    'sample_required_name' => 'Required name',
+    'required' => 'is required',
+
+    /**
+     * Message
+     */
+    'message_update_successfully' => 'Update sample item successfully',
+    'message_add_successfully' => 'Add new sample item successfully',
+    'message_delete_successfully' => 'Delete sample item successfully',
+
+    /**
+     * Validator message
+     */
+    'title_unvalid_length' => 'Unvalid lenght title. Allow from: <b>:TITLE_MIN_LENGTH</b> to <b>:TITLE_MAX_LENGTH</b>.',
+];
---
+++ laravel/packages/foostart/sample/src/lang/vi/sample_admin.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    "package_name" => 'Sample',
+    "package_description" => 'Sample package is for initial',
+    "order" => '#',
+    "operations" => 'Operations',
+
+    /**
+     * Page
+     */
+
+    'page_list' => 'Sample page list',
+    'page_add' => 'Sample page add',
+    'page_edit' => 'Sample page edit',
+    'page_search' => 'Sample page search',
+
+    /**
+     * Menu items & sidebar
+     */
+    "list" => 'Sample list',
+    "add" => 'Add new item',
+
+    /**
+     *
+     */
+    'form_heading' => 'General data',
+    'title' => 'Title',
+
+    /**
+     * Message
+     */
+    'message_update_successfully' => 'Update sample item successfully',
+    'message_add_successfully' => 'Add new sample item successfully',
+    'message_delete_successfully' => 'Delete sample item successfully',
+
+    /**
+     * Validator message
+     */
+    'title_unvalid_length' => 'Unvalid lenght title. Allow from: <b>:TITLE_MIN_LENGTH</b> to <b>:TITLE_MAX_LENGTH</b>.',
+];
