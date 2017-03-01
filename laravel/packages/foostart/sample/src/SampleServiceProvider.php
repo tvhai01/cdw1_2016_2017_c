2c43e763022f35499f8dfb12b4db129336a333fd
tvhai01 <runordie98@gmail.com>
2017-03-01 09:35:32 +0700

    update laravel admin

--- laravel/packages/foostart/sample/src/SampleServiceProvider.php
+++ laravel/packages/foostart/sample/src/SampleServiceProvider.php
@@ -1,20 +1,41 @@
 <?php
 
-namespace foostart\sample;
+namespace Foostart\Sample;
 
 use Illuminate\Support\ServiceProvider;
+use LaravelAcl\Authentication\Classes\Menu\SentryMenuFactory;
 
-class SampleServiceProvider extends ServiceProvider
-{
+use URL, Route;
+use Illuminate\Http\Request;
+
+
+class SampleServiceProvider extends ServiceProvider {
+
     /**
      * Bootstrap the application services.
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-        $this->loadViewsFrom(__DIR__.'/views', 'sample');
+    public function boot(Request $request) {
+        /**
+         * Publish
+         */
+        $this->publishes([
+        ]);
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'sample');
+
+
+        /**
+         * Translations
+         */
+         $this->loadTranslationsFrom(__DIR__.'/lang', 'sample');
+
+
+        /**
+         * Load view composer
+         */
+        $this->sampleViewComposer($request);
     }
 
     /**
@@ -24,8 +45,43 @@ class SampleServiceProvider extends ServiceProvider
      */
     public function register() {
         include __DIR__ . '/routes.php';
-        $this->app->make('Foostart\Sample\SampleController');
+
+        /**
+         * Load controllers
+         */
+        $this->app->make('Foostart\Sample\Controllers\Admin\SampleAdminController');
+
+         /**
+         * Load Views
+         */
+        $this->loadViewsFrom(__DIR__ . '/views', 'sample');
     }
-    
+
+    /**
+     *
+     */
+    public function sampleViewComposer(Request $request) {
+
+        view()->composer('sample::sample*', function ($view) {
+            global $request;
+            $sample_id = $request->get('id');
+            $is_action = empty($sample_id)?'add':'edit';
+
+            $view->with('sidebar_items', [
+
+                //list
+                trans('sample::sample_admin.list') => [
+                    'url' => URL::route('admin_sample'),
+                    "icon" => '<i class="fa fa-users"></i>'
+                ],
+                //add
+                trans('sample::sample_admin.'.$is_action) => [
+                    'url' => URL::route('admin_sample.edit'),
+                    "icon" => '<i class="fa fa-users"></i>'
+                ],
+            ]);
+            //
+        });
+    }
 
 }
--- laravel/packages/foostart/sample/src/routes.php
+++ laravel/packages/foostart/sample/src/routes.php
@@ -1,5 +1,53 @@
 <?php
 
-Route::get('sample', 'Foostart\Sample\SampleController@index');
+use Illuminate\Session\TokenMismatchException;
 
-Route::get('samples', 'Foostart\Sample\SampleController@getData');
+/**
+ * FRONT
+ */
+Route::get('sample', [
+    'as' => 'sample',
+    'uses' => 'Foostart\Sample\Controllers\Front\SampleFrontController@index'
+]);
+
+
+/**
+ * ADMINISTRATOR
+ */
+Route::group(['middleware' => ['web']], function () {
+
+    Route::group(['middleware' => ['admin_logged', 'can_see']], function () {
+
+        /**
+         * list
+         */
+        Route::get('admin/sample', [
+            'as' => 'admin_sample',
+            'uses' => 'Foostart\Sample\Controllers\Admin\SampleAdminController@index'
+        ]);
+
+        /**
+         * edit-add
+         */
+        Route::get('admin/sample/edit', [
+            'as' => 'admin_sample.edit',
+            'uses' => 'Foostart\Sample\Controllers\Admin\SampleAdminController@edit'
+        ]);
+
+        /**
+         * post
+         */
+        Route::post('admin/sample/edit', [
+            'as' => 'admin_sample.post',
+            'uses' => 'Foostart\Sample\Controllers\Admin\SampleAdminController@post'
+        ]);
+
+        /**
+         * delete
+         */
+        Route::get('admin/sample/delete', [
+            'as' => 'admin_sample.delete',
+            'uses' => 'Foostart\Sample\Controllers\Admin\SampleAdminController@delete'
+        ]);
+    });
+});
